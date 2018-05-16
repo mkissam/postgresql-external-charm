1e2c2843076cbcaea1502b6985f58e5f227346c5
Marton Kiss <marton.kiss@gmail.com>
2018-05-16 13:17:34 +0200

    Initial commit

---
+++ reactive/postgresql_external.py
@@ -0,0 +1,17 @@
+from charms.reactive import when, when_not, set_state
+
+
+@when_not('postgresql-external.installed')
+def install_postgresql_external():
+    # Do your setup here.
+    #
+    # If your charm has other dependencies before it can install,
+    # add those as @when() clauses above., or as additional @when()
+    # decorated handlers below
+    #
+    # See the following for information about reactive charms:
+    #
+    #  * https://jujucharms.com/docs/devel/developer-getting-started
+    #  * https://github.com/juju-solutions/layer-basic#overview
+    #
+    set_state('postgresql-external.installed')
